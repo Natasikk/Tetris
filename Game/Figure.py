7dcb3aa369b31a0d4302def2f1bcaf3446e4cb96
Наталья <natali_petrova_04@bk.ru>
2023-11-02 14:21:32 +0300

    Start creating shapes

---
+++ Game/Figure.py
@@ -0,0 +1,7 @@
+from Settings import *
+
+
+class Figure:
+    def __init__(self):
+        self.color = RED
+        self.cords = [(0, 0), (1, 0)]
--- Game/main.py
+++ Game/main.py
@@ -1,5 +1,6 @@
 import pygame
 from Settings import *
+from Figure import Figure
 
 
 class Main:
@@ -8,6 +9,8 @@ class Main:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
         self.run = True
+        self.figures = []
+        self.tick = 0
 
     def events(self):
         for event in pygame.event.get():
@@ -16,8 +19,12 @@ class Main:
 
     def main_loop(self):
         while self.run:
+            self.tick += 1
+            print(self.tick)
             self.clock.tick(FPS)
             pygame.display.set_caption(str(self.clock.get_fps()))
+            if self.tick % 300 == 0:
+                self.figures.append(Figure())
             self.events()
             self.render()
             pygame.display.update()
@@ -26,6 +33,10 @@ class Main:
         for j in range(H):
             for i in range(W):
                 pygame.draw.rect(self.screen, WHITE, (i*TILE, j*TILE, TILE, TILE), 1)
+
+        for item in self.figures:
+            for cords in item.cords:
+                pygame.draw.rect(self.screen, item.color, ((4 + cords[0]) * TILE, cords[1] * TILE, TILE, TILE))
 
 
 main = Main()
